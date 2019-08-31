b8f7b9a288cb0a8ea83011b8c0e92102a1cc186f
ru iz <ruhaim@gmail.com>
2019-08-31 16:12:44 +0530

    lazy-load the modules in angular.json file -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-lazy-component-app';
+  moduleMatrix = [[1,2],[1,2],[1,2]]
+  lazyData = [
+    {
+      label:"Module-1 Comp-1",
+      modulePath:"..path to module"
+    },
+    
+  ]
 }
