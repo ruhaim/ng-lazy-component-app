43112ac0e3873343dfd6a01f8fcd73d905a385c6
ru iz <ruhaim@gmail.com>
2019-08-31 18:53:59 +0530

    change to allow hot loading of libraries at debug time  - https://mereommig.dk/en/blog/adding-livereload-to-the-angular-cli-libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgRuhLazyComponentModule } from 'ng-ruh-lazy-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgRuhLazyComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
