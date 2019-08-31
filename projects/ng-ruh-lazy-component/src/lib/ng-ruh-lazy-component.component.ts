c7dedce2d829c081d2ade5ac73f0754f6a7f9bfa
ru iz <ruhaim@gmail.com>
2019-08-31 18:55:15 +0530

    clean up code for easy readability

--- projects/ng-ruh-lazy-component/src/lib/ng-ruh-lazy-component.component.ts
+++ projects/ng-ruh-lazy-component/src/lib/ng-ruh-lazy-component.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit, NgModuleFactoryLoader, SystemJsNgModuleLoader, Injec
 import { ComponentFactoryResolver, NgModuleRef } from '@angular/core';
 import { Observable } from 'rxjs';
 @Component({
-  selector: 'ng-ruh-ng-ruh-lazy-component',
+  selector: 'ng-ruh-lazy-component',
   template: `
     <ng-content *ngIf="isLoading" select="[loading]">
     </ng-content>
     <ng-container  #vcRefComp 
-                  *ngComponentOutlet="comp; ngModuleFactory: childFactory;" >
+                  *ngComponentOutlet="compType; ngModuleFactory: moduleFactory;" >
     </ng-container>
     <ng-container #vcRef></ng-container>
   `,
@@ -19,10 +19,11 @@ import { Observable } from 'rxjs';
 })
 export class NgRuhLazyComponentComponent implements OnInit {
 
-  childFactory;
+  moduleFactory;
 
-  comp: Type<any>;
+  compType: Type<any>;
   compRef : ComponentRef<any>;
+  componentData :any
   isLoading : boolean
 
   @ViewChild("vcRef", {read: ViewContainerRef}) vcRef:ViewContainerRef;
@@ -35,12 +36,16 @@ export class NgRuhLazyComponentComponent implements OnInit {
   @Input()
   set componentPath(value: string) {
       const [modulePath, moduleName, componentName] = value.split("#")
-      this.loadModule(`${modulePath}#${moduleName}`)
+      this.loadModule(`${modulePath}#${moduleName}`, componentName)
   }
 
   @Input()
   set data(value: object) {
-      this.compRef.instance.data
+      this.componentData = value;
+      if(this.compRef.instance){
+        this.compRef.instance.data = value;
+      }
+
       // this.loadModule(`${modulePath}#${moduleName}`)
   }
 
@@ -53,31 +58,27 @@ export class NgRuhLazyComponentComponent implements OnInit {
 
   }
 
-  async loadModule(path:string){
+  async loadModule(path:string, compName=""){
     this.isLoading = true
-    console.log("Path:" , path)
-    let factory = await this.factoryLoader.load(path)
     // Child module factory.
-    this.childFactory=factory;
+    this.moduleFactory = await this.factoryLoader.load(path)
+    
     // Child module reference.
-    const module:NgModuleRef<any>=factory.create(this.injector);
+    const module:NgModuleRef<any>=this.moduleFactory.create(this.injector);
 
-    const r = module.componentFactoryResolver;
-    const moduleFactories:Map<any, any> = module.componentFactoryResolver["_factories"];
-    let dd = module.instance
+    let moduleInstance = module.instance
 
-    //  this.comp = <Component> moduleFactories.values().next().value
-    // this.comp = module.instance.get("FormItemWrapperComponent2")
-    this.comp=module.instance.getEntry();
+    debugger
+    this.compType=moduleInstance.componentMap[compName] || moduleInstance.getComponentEntry(compName);
 
     //this.childType=module.instance.getEntry();
 
-    let childFactory:ComponentFactory<any>=module.componentFactoryResolver
-    .resolveComponentFactory(this.comp);
+    let componentFactory:ComponentFactory<any>=module.componentFactoryResolver
+    .resolveComponentFactory(this.compType);
     
     // // Child component reference.
-    this.compRef=childFactory.create(this.injector);
-    this.compRef.instance.data = "hjhf"
+    this.compRef=componentFactory.create(this.injector);
+    this.compRef.instance.data = this.componentData;
     // // Inserting a child component into the view.
     this.vcRef.insert(this.compRef.hostView);
     this.isLoading = false
