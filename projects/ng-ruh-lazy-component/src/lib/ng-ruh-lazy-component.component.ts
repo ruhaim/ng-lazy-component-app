79935c3b7d29cdf913d15bd90ed49234490b510d
ru iz <ruhaim@gmail.com>
2019-08-31 22:19:26 +0530

    added basic error handling and validation

--- projects/ng-ruh-lazy-component/src/lib/ng-ruh-lazy-component.component.ts
+++ projects/ng-ruh-lazy-component/src/lib/ng-ruh-lazy-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, NgModuleFactoryLoader, SystemJsNgModuleLoader, Injector, Input, ViewContainerRef, ViewChild, ComponentFactory, Type, ComponentRef } from '@angular/core';
+import { Component, OnInit, NgModuleFactoryLoader, SystemJsNgModuleLoader, 
+        Injector, Input, ViewContainerRef, ViewChild, ComponentFactory, 
+        Type, ComponentRef, NgModuleFactory } from '@angular/core';
 import { ComponentFactoryResolver, NgModuleRef } from '@angular/core';
 import { Observable } from 'rxjs';
 @Component({
@@ -6,9 +8,8 @@ import { Observable } from 'rxjs';
   template: `
     <ng-content *ngIf="isLoading" select="[loading]">
     </ng-content>
-    <ng-container  #vcRefComp 
-                  *ngComponentOutlet="compType; ngModuleFactory: moduleFactory;" >
-    </ng-container>
+    <ng-content *ngIf="error" select="[error]">
+    </ng-content>
     <ng-container #vcRef></ng-container>
   `,
   styles: [],
@@ -19,24 +20,24 @@ import { Observable } from 'rxjs';
 })
 export class NgRuhLazyComponentComponent implements OnInit {
 
-  moduleFactory;
+  moduleFactory:NgModuleFactory<any>;
 
   compType: Type<any>;
   compRef : ComponentRef<any>;
   componentData :any
   isLoading : boolean
+  error = null
 
   @ViewChild("vcRef", {read: ViewContainerRef}) vcRef:ViewContainerRef;
 
-  @Input() formData:Observable<any>;
   @Input()
   set modulePath(value: string) {
       this.loadModule(value)
   }
   @Input()
   set componentPath(value: string) {
-      const [modulePath, moduleName, componentName] = value.split("#")
-      this.loadModule(`${modulePath}#${moduleName}`, componentName)
+    const [modulePath, moduleName, componentName] = value.split("#")
+    this.loadModule(`${modulePath}#${moduleName}`, componentName)
   }
 
   @Input()
@@ -58,30 +59,63 @@ export class NgRuhLazyComponentComponent implements OnInit {
 
   }
 
+  resolveCompType(moduleType, compName:string){
+    let compType=undefined
+    
+    compType = moduleType.componentMap && moduleType.componentMap[compName]
+
+    if(!compType){
+      compType = moduleType.getComponentEntry && moduleType.getComponentEntry(compName)
+
+    }
+
+    if(!compType){
+      throw new Error(
+        `A component reference was not found mapped to requested string '${compName}',
+        make sure the lazy loaded module implements a static 'componentMap' or
+        'getComponentEntry(compName)' on the module root `)
+    }
+
+    return compType;
+  }
   async loadModule(path:string, compName=""){
-    this.isLoading = true
-    // Child module factory.
-    this.moduleFactory = await this.factoryLoader.load(path)
-    
-    // Child module reference.
-    const module:NgModuleRef<any>=this.moduleFactory.create(this.injector);
+    try{
 
-    let moduleInstance = module.instance
+      this.isLoading = true
+      // Child module factory.
+      this.moduleFactory = await this.factoryLoader.load(path)
+      
+      // Child module reference.
+      const module:NgModuleRef<any>=this.moduleFactory.create(this.injector);
+  
+      let moduleInstance = module.instance
+      const moduleType = <any>this.moduleFactory.moduleType
+      this.compType=this.resolveCompType(moduleType, compName)
+  
+      //this.childType=module.instance.getEntry();
+  
+      let componentFactory:ComponentFactory<any>=module.componentFactoryResolver
+          .resolveComponentFactory(this.compType);
+      
+      // // Child component reference.
+      this.compRef=componentFactory.create(this.injector);
+      this.compRef.instance.data = this.componentData;
+      // // Inserting a child component into the view.
+      this.vcRef.clear()
+      this.vcRef.insert(this.compRef.hostView);
+      this.isLoading = false
+    }catch(err){
+      this.error = err
+      throw err
+    }finally{
+      this.isLoading = false
+    }
 
-    debugger
-    this.compType=moduleInstance.componentMap[compName] || moduleInstance.getComponentEntry(compName);
 
-    //this.childType=module.instance.getEntry();
 
-    let componentFactory:ComponentFactory<any>=module.componentFactoryResolver
-    .resolveComponentFactory(this.compType);
-    
-    // // Child component reference.
-    this.compRef=componentFactory.create(this.injector);
-    this.compRef.instance.data = this.componentData;
-    // // Inserting a child component into the view.
-    this.vcRef.insert(this.compRef.hostView);
-    this.isLoading = false
+
+
+
 
   }
 
